4e854e34d62f4699943b09101ecc973a7b3719b7
LilyHuo <Yongjiahuo9210@gmail.com>
2015-12-23 17:12:03 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -5,3 +5,18 @@
 exports.index = function(req, res, next){
   res.render('index', { title: 'Tweet visualization', criteria:'Sentiment' });
 };
+exports.login = function(req, res, next){
+  res.render('login', { title: 'Tweet visualization', criteria:'Sentiment' });
+};
+exports.signup = function(req, res, next){
+  res.render('signup', { title: 'Tweet visualization', criteria:'Sentiment' });
+};
+exports.destination = function(req, res, next){
+  res.render('destination', { title: 'Tweet visualization', criteria:'Sentiment' });
+};
+exports.range = function(req, res, next){
+  res.render('range', { title: 'Tweet visualization', criteria:'Sentiment' });
+};
+exports.file = function(req, res, next){
+  res.render('file', { title: 'Tweet visualization', criteria:'Sentiment' });
+};
