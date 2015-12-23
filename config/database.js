cb42dc4c76f93a2f43f57745f1c1a1fc2a52671e
LilyHuo <Yongjiahuo9210@gmail.com>
2015-12-23 17:17:50 -0500

    Create database.js

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+	'url' : '*'//'dyn-129-236-225-184.dyn.columbia.edu:27000' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
